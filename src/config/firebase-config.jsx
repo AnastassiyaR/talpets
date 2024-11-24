45dfee58de9077cc0a11082418f78da8915e409a
Anastassiya <anastassiya.ryzhko@gmail.com>
2024-11-24 22:52:46 +0200

    firebase config

---
+++ src/config/firebase-config.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAbOkPm-guvv4Zm_PxOYdL6N4dLFZH1xY4",
+  authDomain: "talpets.firebaseapp.com",
+  projectId: "talpets",
+  storageBucket: "talpets.firebasestorage.app",
+  messagingSenderId: "112781167120",
+  appId: "1:112781167120:web:89753203eee4d82b8cd895"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
